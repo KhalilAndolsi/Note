648b123c3fc2e065ff5fe91a35c3daafe41c4de0
rayen_andolsi <rayen.andolsi98@gmail.com>
2023-11-29 18:14:36 +0100

    fix button and add my discreption

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import User from "./components/user/User"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='/' element={<User/>} />
       </Routes>
+      <h1 className='mt-5 p-2 text-center font-semibold'>this app created by <a href='https://github.com/KhalilAndolsi' className=' underline'>khalil andolsi</a> with mush ♥</h1>
     </BrowserRouter>
   );
 }
